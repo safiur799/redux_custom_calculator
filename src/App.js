fef998a44e4e099f28717e4af9e244224452beda
safiur799 <safiurrahaman799@gmail.com>
2021-10-13 08:51:19 +0530

    secondcalculator

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import "./styles.css";
-import { INCREAMENT_COUNTER, DECREAMENT_COUNTER } from "./Redux/actionsTypes";
+import {
+  INCREAMENT_COUNTER,
+  DECREAMENT_COUNTER,
+  ADD_COUNTER,
+  MULTIPLY_COUNTER,
+  SUBTRACT_COUNTER,
+  DIVIDE_COUNTER,
+} from "./Redux/actionsTypes";
 import { useDispatch, useSelector } from "react-redux";
 import counterInc from "./Redux/actions";
+import { useState } from "react";
 
 export default function App() {
   const dispatch = useDispatch();
-
+  const [number, setNumber] = useState();
   const counter = useSelector((state) => state.counter);
   const handleClick = () => {
     const payloadInc = {
       type: INCREAMENT_COUNTER,
-      payload: 1
+      payload: 1,
     };
     const dispatchIncreament = counterInc(payloadInc);
     dispatch(dispatchIncreament);
@@ -18,18 +26,73 @@ export default function App() {
   const handleClick2 = () => {
     const payloadInc = {
       type: DECREAMENT_COUNTER,
-      payload: 1
+      payload: 1,
     };
     const dispatchIncreament = counterInc(payloadInc);
     dispatch(dispatchIncreament);
   };
-
+  const HandlerAdd = () => {
+    const payload = {
+      type: ADD_COUNTER,
+      payload: number,
+    };
+    const dispatchAdd = counterInc(payload);
+    dispatch(dispatchAdd);
+  };
+  const HandlerMul = () => {
+    const payload = {
+      type: MULTIPLY_COUNTER,
+      payload: number,
+    };
+    const dispatchMul = counterInc(payload);
+    dispatch(dispatchMul);
+  };
+  const HandlerSub = () => {
+    const payload = {
+      type: SUBTRACT_COUNTER,
+      payload: number,
+    };
+    const dispatchSub = counterInc(payload);
+    dispatch(dispatchSub);
+  };
+  const HandlerDiv = () => {
+    const payload = {
+      type: DIVIDE_COUNTER,
+      payload: number,
+    };
+    const dipatchDiv = counterInc(payload);
+    dispatch(dipatchDiv);
+  };
   return (
     <div className="App">
       <h1>custom calculator</h1>
       <h1>{counter}</h1>
       <button onClick={handleClick}>increament</button>
       <button onClick={handleClick2}>decreament</button>
+      <br />
+      <div style={{ marginTop: "2rem" }}>
+        <input
+          type="number"
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+          placeholder="enter-number"
+        />
+      </div>
+      <button
+        onClick={HandlerAdd}
+        style={{ marginTop: "1rem", marginRight: "1rem" }}
+      >
+        add
+      </button>
+      <button onClick={HandlerMul}>multiply</button>
+      <br />
+      <button
+        onClick={HandlerSub}
+        style={{ marginTop: "2rem", marginRight: "1rem" }}
+      >
+        subtract
+      </button>
+      <button onClick={HandlerDiv}>divide</button>
     </div>
   );
 }
--- src/Redux/reducer.js
+++ src/Redux/reducer.js
@@ -1,6 +1,13 @@
-import { INCREAMENT_COUNTER, DECREAMENT_COUNTER } from "./actionsTypes";
+import {
+  INCREAMENT_COUNTER,
+  DECREAMENT_COUNTER,
+  ADD_COUNTER,
+  SUBTRACT_COUNTER,
+  MULTIPLY_COUNTER,
+  DIVIDE_COUNTER,
+} from "./actionsTypes";
 const initState = {
-  counter: 0
+  counter: 0,
 };
 
 const reducer = (state = initState, { type, payload }) => {
@@ -8,13 +15,37 @@ const reducer = (state = initState, { type, payload }) => {
     case INCREAMENT_COUNTER: {
       return {
         ...state,
-        counter: state.counter + payload
+        counter: state.counter + payload,
       };
     }
     case DECREAMENT_COUNTER: {
       return {
         ...state,
-        counter: state.counter - payload
+        counter: state.counter - payload,
+      };
+    }
+    case ADD_COUNTER: {
+      return {
+        ...state,
+        counter: state.counter + Number(payload),
+      };
+    }
+    case SUBTRACT_COUNTER: {
+      return {
+        ...state,
+        counter: state.counter - Number(payload),
+      };
+    }
+    case MULTIPLY_COUNTER: {
+      return {
+        ...state,
+        counter: state.counter * payload,
+      };
+    }
+    case DIVIDE_COUNTER: {
+      return {
+        ...state,
+        counter: state.counter / payload,
       };
     }
     default:
